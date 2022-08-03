e85046eef099067ccc4a55f58805caff3a5ac3c6
Mitchell  Lovell <mitchell@mitchells-imac.mynetworksettings.com>
2022-08-03 16:03:18 -0400

    Challenge 15 complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -7,8 +7,21 @@ library(ggplot2)
 
 MechaCar <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F) 
   #Generate Multiple Linear Regression Model
-regression <- lm(mpg ~ vehicle_weight + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar) 
+regression <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, MechaCar) 
 summary(regression)
+regression
+
+yvals <- regression$coefficients['vehicle_weight']*MechaCar$vehicle_weight +
+  regression$coefficients['(Intercept)'] #determine y-axis values from linear model
+plt <- ggplot(MechaCar,aes(x='vehicle_weight',y=mpg)) #import dataset into ggplot2
+plt + geom_point() + geom_line(aes(y=yvals), color = "red") #plot scatter and linear model
 
 
 
+##Deliverable 2
+library(tidyverse)
+suspension_coils <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+total_summary <- suspension_coils  %>% summarize(Mean=mean(PSI), Median= median(PSI), Variance=var(PSI), SD = sd(PSI)) #create summary table with multiple columns  
+total_summary
+lot_summary <- suspension_coils  %>% group_by(Manufacturing_Lot) %>%summarize(Mean=mean(PSI), Median= median(PSI), Variance=var(PSI), SD = sd(PSI)) #create summary table with multiple columns  
+lot_summary
