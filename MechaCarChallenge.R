15c461c4de589d9ddb5a4e61c436325a53371156
Mitchell  Lovell <mitchell@mitchells-imac.mynetworksettings.com>
2022-08-03 16:07:21 -0400

    Challenge 15 Complete

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,9 +1,11 @@
 #Module 15 Challenge
-
-##Deliverable 1
-
 library(dplyr)
 library(ggplot2)
+library(tidyverse)
+
+
+##Deliverable 1: Linear Regression to Predict MPG
+
 
 MechaCar <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F) 
   #Generate Multiple Linear Regression Model
@@ -18,10 +20,23 @@ plt + geom_point() + geom_line(aes(y=yvals), color = "red") #plot scatter and li
 
 
 
-##Deliverable 2
-library(tidyverse)
+##Deliverable 2: Summary Statistics on Suspension Coils
 suspension_coils <- read.csv('Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
 total_summary <- suspension_coils  %>% summarize(Mean=mean(PSI), Median= median(PSI), Variance=var(PSI), SD = sd(PSI)) #create summary table with multiple columns  
 total_summary
 lot_summary <- suspension_coils  %>% group_by(Manufacturing_Lot) %>%summarize(Mean=mean(PSI), Median= median(PSI), Variance=var(PSI), SD = sd(PSI)) #create summary table with multiple columns  
 lot_summary
+
+##Deliverable 3: T-Tests on Suspension Coils
+results <- t.test(suspension_coils$PSI, mu = 1500)  #compare sample versus population means
+re1 <- t.test(subset(suspension_coils,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+re2 <- t.test(subset(suspension_coils,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+re3 <- t.test(subset(suspension_coils,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
+
+results
+re1[3]
+re2[3]
+re3[3]
+
+
+
